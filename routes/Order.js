26ae63e0ac7f188f2d2aa415fb5762478998657c
tangent_veil <anirudha0789@gmail.com>
2024-08-14 15:22:50 +0530

    Order controller updates

--- routes/Order.js
+++ routes/Order.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { auth, isPartner, isUser, isAdmin } = require("../middlewares/auth");
-const { placeOrder, purchaseService } = require("../controllers/OrderController");
+const {
+  placeOrder,
+  purchaseService,
+  getUserOrders,
+} = require("../controllers/OrderController");
 
 router.post("/placeOrder", auth, isAdmin, placeOrder);
 router.post("/purchaseService", auth, isAdmin, purchaseService);
+router.get("/getUserOrders", auth, isAdmin, getUserOrders);
 
 module.exports = router;
