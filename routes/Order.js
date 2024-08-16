0f9dcd9f060e991374c063cb5bfd8ee5ff395dea
tangent_veil <anirudha0789@gmail.com>
2024-08-16 08:30:04 +0530

    get all orders for admin

--- routes/Order.js
+++ routes/Order.js
@@ -5,10 +5,12 @@ const {
   placeOrder,
   purchaseService,
   getUserOrders,
+  getAllOrders
 } = require("../controllers/OrderController");
 
 router.post("/placeOrder", auth, isAdmin, placeOrder);
 router.post("/purchaseService", auth, isAdmin, purchaseService);
-router.get("/getUserOrders", auth, isAdmin, getUserOrders);
+router.get("/getUserOrders", auth, isUser, getUserOrders);
+router.get("/getAllOrders", auth, isAdmin, getAllOrders);
 
 module.exports = router;
