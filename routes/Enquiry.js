9267ecdc01d1b40e77868281a59ed1777d227b2a
aditya10_10 <ascs25835@gmail.com>
2024-10-04 14:28:58 +0530

    only basic idea of enquiry and contact has been added for admin

--- routes/Enquiry.js
+++ routes/Enquiry.js
@@ -1,9 +1,18 @@
 const express = require("express");
-const { createEnquiry, getAllEnquiries, getEnquiryById } = require("../controllers/EnquiryController");
+const {
+  createEnquiry,
+  getAllEnquiries,
+  getEnquiryById,
+  updateEnquiry,
+  deleteEnquiry,
+} = require("../controllers/EnquiryController");
+const { auth, isAdmin } = require("../middlewares/auth");
 const router = express.Router();
 
 router.post("/enquire", createEnquiry);
-router.get("/getAllEnquiries", getAllEnquiries);
-router.post("/getEnquiryById", getEnquiryById);
+router.get("/getAllEnquiries", auth, isAdmin, getAllEnquiries);
+router.post("/getEnquiryById", auth, isAdmin, getEnquiryById);
+router.patch("/updateEnquiry/:id", auth, isAdmin, updateEnquiry);
+router.delete("/deleteEnquiry/:id", auth, isAdmin, deleteEnquiry);
 
 module.exports = router;
