ad1cb0845b225b38465f3456baf31e59624a3b26
Anurag Patel <118664591+anurag-2024@users.noreply.github.com>
2024-11-07 19:37:51 +0530

    Changes to blog controller

--- index.js
+++ index.js
@@ -38,7 +38,9 @@ database.connect();
 // Initialize FirebaseAdmin
 
 // Apply middleware
-app.use(express.json());
+app.use(express.json({
+  limit: "50mb",
+}));
 app.use(cookieParser());
 app.use(morgan("tiny"));
 // CORS
